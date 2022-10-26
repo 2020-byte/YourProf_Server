1a1f904f50ec4d7d3bca93dde6729f992d7ed723
2020-byte <gmmoon0824@gmail.com>
2022-10-25 23:07:36 -0700

    send userInfo to client:

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,10 +5,12 @@ import * as userRepository from '../data/auth.js';
 const AUTH_ERROR = { message: 'Authentication Error' };
 
 export const isAuth = async (req, res, next) => {
+    
     const authHeader = req.get('Authorization');
     if (!(authHeader && authHeader.startsWith('Bearer '))) {
         return res.status(401).json(AUTH_ERROR);
     }
+    
 
     const token = authHeader.split(' ')[1];
     jwt.verify(token, config.jwt.secretKey, async (error, decoded) => {
